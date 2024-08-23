4a7739ace436dd45916ff618c87e5f249269a62c
orca-pet3910 <milosz.libiszewski@gmail.com>
2024-08-23 20:08:35 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+// server.js
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  ws.on('message', (message) => {
+    console.log(`Received message => ${message}`);
+    wss.clients.forEach((client) => {
+      client.send(message);
+    });
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
